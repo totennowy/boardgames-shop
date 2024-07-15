e4d20faf8fc67186983706d2ad09e8ce80b26c98
Bartek <depciuch322@gmail.com>
2024-07-15 17:37:57 +0200

    Remove unsupported useCreateIndex option from MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,12 +5,12 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
         });
+        console.log('MongoDB connected');
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
     }
-}
+};
 
 module.exports = connectDB;
