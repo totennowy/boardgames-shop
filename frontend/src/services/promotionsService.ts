415d2f28657185b7679cb3b22fdbfade58205f01
Bartek <depciuch322@gmail.com>
2024-07-29 22:41:10 +0200

    Create a promotions service

---
+++ frontend/src/services/promotionsService.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_UR;
+
+export const getPromotions = async (limit = 12, offset = 0) => {
+  const response = await axios.get(`${API_URL}/promotions`, {
+    params: {
+      limit,
+      offset,
+    },
+  });
+  return response.data;
+};
