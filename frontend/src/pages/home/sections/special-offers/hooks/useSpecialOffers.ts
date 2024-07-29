1d932f6211a9489891f5ef0d7ed85b7f94b2da43
Bartek <depciuch322@gmail.com>
2024-07-29 22:41:23 +0200

    Create a promotions hook

---
+++ frontend/src/pages/home/sections/special-offers/hooks/useSpecialOffers.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import { useAPIContext } from '@contexts/useAPIContext';
+import { getPromotions } from '@services/promotionsService';
+
+const useSpecialOffers = (limit: number, offset: number) => {
+  const { setPromotionsData } = useAPIContext();
+  const [loadingData, setLoadingData] = useState<boolean>(true);
+  const [fetchError, setFetchError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchPromotionsData = async () => {
+      try {
+        const data = await getPromotions(limit, offset);
+        setPromotionsData(data);
+      } catch (error) {
+        setFetchError(error as Error);
+      } finally {
+        setLoadingData(false);
+      }
+    };
+
+    fetchPromotionsData();
+  }, [setPromotionsData, limit, offset]);
+
+  return {
+    loadingData,
+    fetchError,
+  };
+};
+
+export default useSpecialOffers;
