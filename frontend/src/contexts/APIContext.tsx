6a5006a7776f3c327734eb077030a4339580d611
Bartek <depciuch322@gmail.com>
2024-07-29 21:56:44 +0200

    Add separate model file to context

--- frontend/src/contexts/APIContext.tsx
+++ frontend/src/contexts/APIContext.tsx
@@ -1,26 +1,11 @@
-import React, { createContext, useState, ReactNode } from 'react';
-
-export type ModelProduct = {
-  id: string;
-  name: string;
-  price: number;
-  releaseDate: string;
-  iconImage: string;
-  stock: number;
-  discount: number;
-  sold: number;
-};
-
-type ModelAPIContext = {
-  bestsellersData: ModelProduct[];
-  setBestsellersData: React.Dispatch<React.SetStateAction<ModelProduct[]>>;
-};
+import React, { createContext, useState } from 'react';
+import {
+  ModelAPIContext,
+  ModelAPIProvider,
+  ModelProduct,
+} from './modelAPIContext';
 
 export const APIContext = createContext<ModelAPIContext | undefined>(undefined);
-
-type ModelAPIProvider = {
-  children: ReactNode;
-};
 
 export const APIProvider: React.FC<ModelAPIProvider> = ({ children }) => {
   const [bestsellersData, setBestsellersData] = useState<ModelProduct[]>([]);
---
+++ frontend/src/contexts/modelAPIContext.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+
+export type ModelProduct = {
+  id: string;
+  name: string;
+  price: number;
+  releaseDate: string;
+  iconImage: string;
+  stock: number;
+  discount: number;
+  sold: number;
+};
+
+export type ModelAPIContext = {
+  bestsellersData: ModelProduct[];
+  setBestsellersData: React.Dispatch<React.SetStateAction<ModelProduct[]>>;
+};
+
+export type ModelAPIProvider = {
+  children: ReactNode;
+};
