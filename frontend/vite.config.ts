6bf556f7a89e3196a9125c816a588700da6d1aa6
Bartek <depciuch322@gmail.com>
2024-07-31 21:49:35 +0200

    Add utils to alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       '@layout': path.resolve(__dirname, 'src/layout'),
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@services': path.resolve(__dirname, 'src/services'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
     },
   },
 });
