1529585c0543ab83736fcf61a7c8c4167cacaf34
Bartek <depciuch322@gmail.com>
2024-08-01 12:55:41 +0200

    Add CardLabels component to manage product labels

---
+++ frontend/src/components/product-card/components/CardLabels.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Label from './Label';
+import { Box } from '@mui/material';
+import { ModelCardLabels } from '../types/modelCardLabels';
+
+const CardLabels: React.FC<ModelCardLabels> = ({ labels }) => {
+  return (
+    <Box
+      sx={{
+        position: 'absolute',
+        top: '8px',
+        left: '0px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '10px',
+      }}
+    >
+      {labels.map((label, index) => (
+        <Label key={index} type={label.type} discount={label.discount} />
+      ))}
+    </Box>
+  );
+};
+
+export default CardLabels;
