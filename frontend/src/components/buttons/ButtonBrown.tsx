8383a174176df374902a2f7189d2f48668f12c45
Bartek <depciuch322@gmail.com>
2024-08-03 22:09:15 +0200

    Improved BrownButton for when product stock is zero or when the product has not been released

--- frontend/src/components/buttons/ButtonBrown.tsx
+++ frontend/src/components/buttons/ButtonBrown.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import { Button } from '@mui/material';
-import { ModelButtonBrown } from './modelButtonBrown';
+import { ModelButtonBrown } from './types/modelButtonBrown';
+import useButtonBrown from './hooks/useButtonBrown';
 
-const ButtonBrown: React.FC<ModelButtonBrown> = ({ icon, onClick, text }) => {
+const ButtonBrown: React.FC<ModelButtonBrown> = ({
+  icon,
+  onClick,
+  text,
+  stock,
+  releaseDate,
+}) => {
+  const {
+    buttonText,
+    isButtonDisabled,
+    commonStyles,
+    enabledStyles,
+    disabledStyles,
+  } = useButtonBrown(text, stock, releaseDate);
+
   return (
     <Button
       startIcon={icon}
-      onClick={onClick}
+      onClick={isButtonDisabled ? undefined : onClick}
       sx={{
-        height: '40px',
-        width: '100%',
-        fontSize: '14px',
-        fontWeight: '600',
-        background: '#614338',
-        color: '#e7e7e7',
-        borderRadius: '0px 0px 4px 4px',
-        transition: '0.2s ease-in-out',
-        '&:hover': {
-          backgroundColor: '#e7e7e7',
-          color: '#614338',
-          border: '2px solid #614338',
-        },
+        ...commonStyles,
+        ...(isButtonDisabled ? disabledStyles : enabledStyles),
       }}
     >
-      {text}
+      {buttonText}
     </Button>
   );
 };
