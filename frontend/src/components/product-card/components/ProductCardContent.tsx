2a9db6730bd203815efca1f950b392f8c240be31
Bartek <depciuch322@gmail.com>
2024-08-05 22:02:31 +0200

    Update ProductCardContent to include current price and line-through for discount

--- frontend/src/components/product-card/components/ProductCardContent.tsx
+++ frontend/src/components/product-card/components/ProductCardContent.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { CardContent, Typography } from '@mui/material';
+import { Box, CardContent, Typography } from '@mui/material';
 import { ModelProductCardContent } from '../types/modelProductCardContent';
+import { useProductCardContent } from '../hooks/useProductCardContent';
 
 const ProductCardContent: React.FC<ModelProductCardContent> = ({
   name,
   price,
+  discount,
   isHovered,
 }) => {
+  const { currentPrice } = useProductCardContent(price, discount);
+
   return (
     <>
       <CardContent
@@ -29,9 +33,44 @@ const ProductCardContent: React.FC<ModelProductCardContent> = ({
         <Typography fontSize={20} fontWeight={700} textAlign="center">
           {name}
         </Typography>
-        <Typography fontSize={16} fontWeight={700}>
-          {price && `$ ${price}`}
-        </Typography>
+
+        {discount > 0 ? (
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              gap: '10px',
+            }}
+          >
+            <Typography
+              fontSize={16}
+              fontWeight={700}
+              sx={{
+                position: 'relative',
+                '&::before': {
+                  content: '""',
+                  position: 'absolute',
+                  left: '-4px',
+                  right: '-4px',
+                  top: '50%',
+                  borderBottom: `2px solid white`,
+                  transform: 'translateY(-50%)',
+                  zIndex: 1,
+                },
+              }}
+            >
+              {price && `$ ${price}`}
+            </Typography>
+            <Typography fontSize={16} fontWeight={700} color="#A03D36">
+              {price && `$ ${currentPrice}`}
+            </Typography>
+          </Box>
+        ) : (
+          <Typography fontSize={16} fontWeight={700}>
+            {price && `$ ${currentPrice}`}
+          </Typography>
+        )}
       </CardContent>
     </>
   );
