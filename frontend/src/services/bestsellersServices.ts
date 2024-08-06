84a67229838dd0f8ae5854fd4f37c8688603a126
Bartek <depciuch322@gmail.com>
2024-08-06 19:07:20 +0200

    Move API config to services folder and refactor service functions to use common fetch logic

--- frontend/src/services/bestsellersServices.ts
+++ frontend/src/services/bestsellersServices.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-const API_URL = import.meta.env.VITE_API_URL;
+import { API_URL } from './apiConfig';
 
 export const getBestsellers = async (limit = 12, offset = 0) => {
   const response = await axios.get(`${API_URL}/bestsellers`, {
--- frontend/src/services/promotionsService.ts
+++ frontend/src/services/promotionsService.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-const API_URL = import.meta.env.VITE_API_URL;
+import { API_URL } from './apiConfig';
 
 export const getPromotions = async (limit = 12, offset = 0) => {
   const response = await axios.get(`${API_URL}/promotions`, {
