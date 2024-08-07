84f1f71181ab36b4fa4e5ffa06d927331b93bd9b
Bartek <depciuch322@gmail.com>
2024-08-07 21:31:05 +0200

    Update Bestsellers to use ProductSection

--- frontend/src/pages/home/sections/bestsellers/Bestsellers.tsx
+++ frontend/src/pages/home/sections/bestsellers/Bestsellers.tsx
@@ -1,40 +1,18 @@
-import ProductCarousel from '@components/product-carousel/ProductCarousel';
-import { Box, Typography } from '@mui/material';
 import { useBestsellers } from './hooks/useBestsellers';
 import { useAPIContext } from '@contexts/useAPIContext';
+import ProductsSection from '@components/products-section/ProductsSection';
 
 const Bestsellers = () => {
   const { bestsellersData } = useAPIContext();
   const { loadingData, fetchError } = useBestsellers(18, 0);
 
   return (
-    <Box
-      sx={{
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '40px',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Box sx={{ width: '100%', background: '#614338', padding: '4px 0' }}>
-        <Typography
-          fontSize={32}
-          fontWeight={700}
-          color="#e7e7e7"
-          textAlign="center"
-          textTransform="uppercase"
-        >
-          Bestsellers
-        </Typography>
-      </Box>
-      <ProductCarousel
-        products={bestsellersData}
-        loading={loadingData}
-        error={fetchError}
-      />
-    </Box>
+    <ProductsSection
+      title="Bestsellers"
+      productsData={bestsellersData}
+      loading={loadingData}
+      error={fetchError}
+    />
   );
 };
 
