f57d50ced192d627546934137022ff150b4355d4
Bartek <depciuch322@gmail.com>
2024-08-07 21:31:18 +0200

    Update SpecialOffers to use ProductSection

--- frontend/src/pages/home/sections/special-offers/SpecialOffers.tsx
+++ frontend/src/pages/home/sections/special-offers/SpecialOffers.tsx
@@ -1,40 +1,18 @@
-import ProductCarousel from '@components/product-carousel/ProductCarousel';
 import { useAPIContext } from '@contexts/useAPIContext';
-import { Box, Typography } from '@mui/material';
 import { useSpecialOffers } from './hooks/useSpecialOffers';
+import ProductsSection from '@components/products-section/ProductsSection';
 
 const SpecialOffers = () => {
   const { promotionsData } = useAPIContext();
   const { loadingData, fetchError } = useSpecialOffers(18, 0);
 
   return (
-    <Box
-      sx={{
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '40px',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Box sx={{ width: '100%', background: '#614338', padding: '4px 0' }}>
-        <Typography
-          fontSize={32}
-          fontWeight={700}
-          color="#e7e7e7"
-          textAlign="center"
-          textTransform="uppercase"
-        >
-          Special offers
-        </Typography>
-      </Box>
-      <ProductCarousel
-        products={promotionsData}
-        loading={loadingData}
-        error={fetchError}
-      />
-    </Box>
+    <ProductsSection
+      title="Special Offers"
+      productsData={promotionsData}
+      loading={loadingData}
+      error={fetchError}
+    />
   );
 };
 
