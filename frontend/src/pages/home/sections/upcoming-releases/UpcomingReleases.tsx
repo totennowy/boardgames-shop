ef269529f0b22a3b3099be5d2e70a8ce7e9c3cda
Bartek <depciuch322@gmail.com>
2024-08-07 21:31:23 +0200

    Update UpcomingReleases to use ProductSection

--- frontend/src/pages/home/sections/upcoming-releases/UpcomingReleases.tsx
+++ frontend/src/pages/home/sections/upcoming-releases/UpcomingReleases.tsx
@@ -1,40 +1,18 @@
-import ProductCarousel from '@components/product-carousel/ProductCarousel';
 import { useAPIContext } from '@contexts/useAPIContext';
-import { Box, Typography } from '@mui/material';
 import { useUpcomingReleases } from './hooks/useUpcomingReleases';
+import ProductsSection from '@components/products-section/ProductsSection';
 
 const UpcomingReleases = () => {
   const { upcomingReleasesData } = useAPIContext();
   const { loadingData, fetchError } = useUpcomingReleases(12, 0);
 
   return (
-    <Box
-      sx={{
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '40px',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Box sx={{ width: '100%', background: '#614338', padding: '4px 0' }}>
-        <Typography
-          fontSize={32}
-          fontWeight={700}
-          color="#e7e7e7"
-          textAlign="center"
-          textTransform="uppercase"
-        >
-          Upcoming Releases
-        </Typography>
-      </Box>
-      <ProductCarousel
-        products={upcomingReleasesData}
-        loading={loadingData}
-        error={fetchError}
-      />
-    </Box>
+    <ProductsSection
+      title="Upcoming Releases"
+      productsData={upcomingReleasesData}
+      loading={loadingData}
+      error={fetchError}
+    />
   );
 };
 
