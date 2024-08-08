4860768ddfb405e877314f9f4ba69dc48bccbaea
Bartek <depciuch322@gmail.com>
2024-08-08 21:46:33 +0200

    Add react-router-dom to package.json and implement routing in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import Layout from '@layout/Layout';
-import Home from '@pages/home/Home';
 import theme from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { APIProvider } from '@contexts/APIContext';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PageHome from '@pages/home/pageHome';
+import PageProduct from '@pages/product/pageProduct';
 
 function App() {
   return (
@@ -10,7 +12,15 @@ function App() {
       <CssBaseline>
         <APIProvider>
           <Layout>
-            <Home />
+            <Router>
+              <Routes>
+                <Route path="/" element={<PageHome />} />
+                <Route
+                  path="/product/:productId/:productName"
+                  element={<PageProduct />}
+                />
+              </Routes>
+            </Router>
           </Layout>
         </APIProvider>
       </CssBaseline>
