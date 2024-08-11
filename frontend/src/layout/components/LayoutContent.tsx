8e72ff3771fad04747738df48d36f7457336fdc3
Bartek <depciuch322@gmail.com>
2024-08-11 23:50:30 +0200

    Small improvement in LayoutContent

--- frontend/src/layout/components/LayoutContent.tsx
+++ frontend/src/layout/components/LayoutContent.tsx
@@ -1,12 +1,17 @@
+import { ModelLayoutContent } from '@layout/types/layout';
+import { Box } from '@mui/material';
+import React from 'react';
 
-import { ModelLayoutContent } from '@layout/types/layout'
-import { Box } from '@mui/material'
-import React from 'react'
-
-const LayoutContent: React.FC<ModelLayoutContent> = ({children}) => {
+const LayoutContent: React.FC<ModelLayoutContent> = ({ children }) => {
   return (
-    <Box sx={{flex: 1}} >{children}</Box>
-  )
-}
+    <Box
+      sx={{
+        flex: 1,
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
 
-export default LayoutContent
+export default LayoutContent;
