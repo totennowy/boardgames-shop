57aeeacf387987b43874929337b14e37a1b8b251
Bartek <depciuch322@gmail.com>
2024-08-15 23:14:01 +0200

    implement product detail page with dynamic attributes and rating

--- frontend/src/pages/product/PageProduct.tsx
+++ frontend/src/pages/product/PageProduct.tsx
@@ -6,6 +6,7 @@ import GroupIcon from '@mui/icons-material/Group';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import Face4Icon from '@mui/icons-material/Face4';
 import DifficultyBox from './components/DifficultyBox';
+import ProductAttributeBox from './components/ProductAttributeBox';
 
 const PageProduct: React.FC = () => {
   const { productByIdData, loadingData, fetchError } = useProductById();
@@ -87,83 +88,23 @@ const PageProduct: React.FC = () => {
               borderBottom: '1px solid #d3d3d3',
             }}
           >
-            <Box
-              sx={{
-                width: '100%',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                gap: '6px',
-                padding: '6px',
-                borderRight: '1px solid #d3d3d3',
-              }}
-            >
-              <GroupIcon sx={{ width: 'auto', height: '60px' }} />
-              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <Typography
-                  textTransform={'capitalize'}
-                  fontSize={16}
-                  fontWeight={500}
-                >
-                  {productByIdData.players}
-                </Typography>
-                <Typography fontSize={14} fontWeight={400}>
-                  Best for: {productByIdData.bestPlayers} players
-                </Typography>
-              </Box>
-            </Box>
+            <ProductAttributeBox
+              icon={<GroupIcon sx={{ width: 'auto', height: '60px' }} />}
+              upperLabel={productByIdData.players}
+              lowerLabel={` Best for: ${productByIdData.bestPlayers} players`}
+            />
 
-            <Box
-              sx={{
-                width: '100%',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                gap: '6px',
-                padding: '6px',
-                borderRight: '1px solid #d3d3d3',
-              }}
-            >
-              <AccessTimeIcon sx={{ width: 'auto', height: '60px' }} />
-              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <Typography
-                  textTransform={'capitalize'}
-                  fontSize={16}
-                  fontWeight={500}
-                >
-                  60 - 120 min
-                </Typography>
-                <Typography fontSize={14} fontWeight={400}>
-                  Playing Time
-                </Typography>
-              </Box>
-            </Box>
+            <ProductAttributeBox
+              icon={<AccessTimeIcon sx={{ width: 'auto', height: '60px' }} />}
+              upperLabel="60 - 120 min"
+              lowerLabel="Playing Time"
+            />
 
-            <Box
-              sx={{
-                width: '100%',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                gap: '6px',
-                padding: '6px',
-                borderRight: '1px solid #d3d3d3',
-              }}
-            >
-              <Face4Icon sx={{ width: 'auto', height: '60px' }} />
-              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <Typography
-                  textTransform={'capitalize'}
-                  fontSize={16}
-                  fontWeight={500}
-                >
-                  {productByIdData.age} +
-                </Typography>
-                <Typography fontSize={14} fontWeight={400}>
-                  Suggested Age
-                </Typography>
-              </Box>
-            </Box>
+            <ProductAttributeBox
+              icon={<Face4Icon sx={{ width: 'auto', height: '60px' }} />}
+              upperLabel={`${productByIdData.age} +`}
+              lowerLabel="Playing Time"
+            />
             <DifficultyBox difficulty={productByIdData.difficulty} />
           </Box>
 
