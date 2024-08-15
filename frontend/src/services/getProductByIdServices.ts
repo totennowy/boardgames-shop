fee407e54782cd9be691fef9e2102f530ffd8a49
Bartek <depciuch322@gmail.com>
2024-08-15 23:13:55 +0200

    update product fetch service to align with new API structure

--- frontend/src/services/getProductByIdServices.ts
+++ frontend/src/services/getProductByIdServices.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { API_URL } from './apiConfig';
 
 export const getProductById = async (id: string) => {
-  const response = await axios.get(`${API_URL}/products/${id}`, {});
+  const response = await axios.get(`${API_URL}/${id}`, {});
   return response.data;
 };
