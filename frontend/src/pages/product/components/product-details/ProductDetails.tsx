b602148c6b9633fb6e4badef3370f01188c60683
Bartek <depciuch322@gmail.com>
2024-08-23 15:45:02 +0200

    Improved styles in ProductDetails

--- frontend/src/pages/product/components/product-details/ProductDetails.tsx
+++ frontend/src/pages/product/components/product-details/ProductDetails.tsx
@@ -4,14 +4,27 @@ import { ModelProductDetails } from './types/modelProductDetails';
 
 const ProductDetails: React.FC<ModelProductDetails> = ({ productByIdData }) => {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '8px',
+      }}
+    >
       <Box sx={{ display: 'flex', gap: '6px' }}>
         <Typography>Author:</Typography>
         <Typography fontWeight={300}>
           {productByIdData.author.join(', ')}
         </Typography>
       </Box>
-      <Box sx={{ display: 'flex', gap: '6px' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          gap: '6px',
+          borderBottom: '1px solid #d3d3d3',
+          paddingBottom: '16px',
+        }}
+      >
         <Typography>Illustration:</Typography>
         <Typography fontWeight={300}>
           {productByIdData.illustrations.join(', ')}
